05c98e361825e6e2a116f0ae8efaeedd1118bc0a
Filipe Scaglia <filipecscaglia@gmail.com>
2021-01-07 21:04:58 -0300

    Cart operations

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('5ff74bb26e230b4d2b129ece')
     .then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err => console.log(err));

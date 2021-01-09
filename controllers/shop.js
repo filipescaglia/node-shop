96be1251426774cfbfac17e2e6c85219a9339fc0
Filipe Scaglia <filipecscaglia@gmail.com>
2021-01-09 16:13:52 -0300

    Orders operations

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
     Product.find()
@@ -78,23 +79,45 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
     req.user
-    .addOrder()
-    .then(result => {
-        res.redirect('/orders');
-    })
-    .catch(err => console.log(err));
+        .populate('cart.items.productId')
+        .execPopulate()
+        .then(user => {
+            const products = user.cart.items.map(i => {
+                return {
+                    quantity: i.quantity,
+                    product: {
+                        ...i.productId._doc
+                    }
+                };
+            });
+            const order = new Order({
+                products: products,
+                user: {
+                    name: req.user.name,
+                    userId: req.user
+                }
+            });
+            return order.save();
+        })
+        .then(result => {
+            return req.user.clearCart();
+        })
+        .then(result => {            
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user.getOrders()
-    .then(orders => {
-        res.render('shop/orders', {
-            orders: orders,
-            pageTitle: 'Your Orders',
-            path: '/orders',
-        });
-    })
-    .catch(err => console.log(err));    
+    Order
+        .find({"user.userId": req.user._id})
+        .then(orders => {
+            res.render('shop/orders', {
+                orders: orders,
+                pageTitle: 'Your Orders',
+                path: '/orders',
+            });
+        })
+        .catch(err => console.log(err));    
 };
